e236c0bd18a4cdc88295c499fb4c88a5f6d19662
TeTeT Nimitz <tetet.nimitz@gmail.com>
2024-05-15 01:43:56 +0200

    Add vwv Main, update docs

---
+++ TeTeT VWV/mods/tech/vwv Main/README.md
@@ -0,0 +1,4 @@
+# Vietnam War Vessels mod, main
+Misc files for the Vietnam Wasr Vessels mod
+
+Thanks a lot to coleminor for the graphics for the mod. Set the wallpaper for VWV and see his creation.
---
+++ TeTeT VWV/mods/tech/vwv Main/entry.lua
@@ -0,0 +1,23 @@
+declare_plugin("tetet_vwv_main",
+{
+	dirName		  = current_mod_path,
+	displayName   = _("Vietnam War Vessels"),
+	shortName	  = "vwv",
+	version		  = "0.5.0",
+	state		  = "installed",
+	fileMenuName  = _("vwv"),
+	developerName = "TeTeT",
+	info		  = _("Vietnam War Vessels mod"),
+	-- encyclopedia_path = current_mod_path .. '/Encyclopedia',
+
+	Skins =
+	{
+		{
+			name  = "Vietnam War Vessels",
+			dir   = "Skins/1"
+		},
+	},
+
+})
+
+plugin_done()
