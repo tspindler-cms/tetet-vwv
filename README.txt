4c8f50fd8cfe7f6ec5f70e694f72cdb96148d94b
TeTeT Nimitz <tetet.nimitz@gmail.com>
2024-07-14 11:02:39 +0200

    Prepare 0.5.3 update

--- README.txt
+++ README.txt
@@ -1,5 +1,5 @@
 Vietnam War Vessels by TeTeT
-0.5.2
+0.5.3
 
 This mod is released under the Creative Commons Attribution-NonCommercial-ShareAlike license, see LICENSE.md for details
 
--- TeTeT VWV/mods/tech/vwv USS Bon Homme Richard CVA-31/Database/cva-31.lua
+++ TeTeT VWV/mods/tech/vwv USS Bon Homme Richard CVA-31/Database/cva-31.lua
@@ -119,11 +119,11 @@ GT.carrierIlluminationStates = {
 								
 --Damage Model
 GT.DM = {
-    { area_name = "Deck aft",              area_arg = 70, area_life = 300, armour = {width = 0.030}, area_fire = { connector = "FIRE_DECK_AFT", size = 0.8 }, belongsToRunway = true},
-    { area_name = "Deck cat",              area_arg = 71, area_life = 300, armour = {width = 0.030}, area_fire = { connector = "FIRE_DECK_CAT", size = 0.8 }, belongsToRunway = true},
-    { area_name = "Deck fore",             area_arg = 72, area_life = 300, armour = {width = 0.030}, area_fire = { connector = "FIRE_DECK_FORE", size = 0.8 }, belongsToRunway = true},
-    { area_name = "Deck island",           area_arg = 73, area_life = 300, armour = {width = 0.030}, area_fire = { connector = "FIRE_DECK_ISLAND", size = 0.8 }, belongsToRunway = true},
-	{ area_name = "Deck ele",              area_arg = 74, area_life = 300, armour = {width = 0.030}, area_fire = { connector = "FIRE_DECK_CAT", size = 0.8 }, belongsToRunway = true},
+    { area_name = "PALUBA_aft",            area_arg = 70, area_life = 300, armour = {width = 0.030}, area_fire = { connector = "FIRE_DECK_AFT", size = 0.8 }, belongsToRunway = true},
+    { area_name = "PALUBA_cat",            area_arg = 71, area_life = 300, armour = {width = 0.030}, area_fire = { connector = "FIRE_DECK_CAT", size = 0.8 }, belongsToRunway = true},
+    { area_name = "PALUBA_fore",           area_arg = 72, area_life = 300, armour = {width = 0.030}, area_fire = { connector = "FIRE_DECK_FORE", size = 0.8 }, belongsToRunway = true},
+    { area_name = "PALUBA_island",         area_arg = 73, area_life = 300, armour = {width = 0.030}, area_fire = { connector = "FIRE_DECK_ISLAND", size = 0.8 }, belongsToRunway = true},
+	{ area_name = "PALUBA_ele",            area_arg = 74, area_life = 300, armour = {width = 0.030}, area_fire = { connector = "FIRE_DECK_CAT", size = 0.8 }, belongsToRunway = true},
 	{ area_name = "Deck LSO",              area_arg = 75, area_life = 50 },
     { area_name = "Hull Simple",           area_arg = 76, area_life = 150 },
     { area_name = "Island",                area_arg = 77, area_life = 150 },
