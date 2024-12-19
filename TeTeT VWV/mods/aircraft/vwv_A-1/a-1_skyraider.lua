687faa20656b38d79e269a8e799a510f3bb604d0
TeTeT Nimitz <tetet.nimitz@gmail.com>
2024-12-19 01:23:28 +0100

    A-1 Skyraider     - New collision shell     - Improve model     - Fix bridle use     - Fix night time and gear problem

--- TeTeT VWV/mods/aircraft/vwv_A-1/a-1_skyraider.lua
+++ TeTeT VWV/mods/aircraft/vwv_A-1/a-1_skyraider.lua
@@ -15,7 +15,7 @@ local vwv_a1_skyraider =  {
 	{
 		{
 			file  	 	= 'a_1_skyraider',
-			life  	 	= 30, -- lifebar
+			life  	 	= 25, -- lifebar
 			vis   	 	= 3, -- visibility gain.
 			desrt    	= 'a_1_skyraider_destroyed', -- Name of destroyed object file name
 			fire  	 	= { 200, 1},
@@ -61,8 +61,8 @@ local vwv_a1_skyraider =  {
 	nose_gear_hiking				=  0.0,
 
 	main_gear_amortizer_direct_stroke	 	=   0, --  down from main_gear_pos !!!
-	main_gear_amortizer_reversal_stroke  	    	=  -0.1, --  -1.0, --  -1
-	main_gear_amortizer_normal_weight_stroke    	=   -0.015,--  -0.05
+	main_gear_amortizer_reversal_stroke  	    	= 0, -- -0.1, --  -1.0, --  -1
+	main_gear_amortizer_normal_weight_stroke    	= 0, --  -0.015,--  -0.05
 
 	wing_tip_pos = {-0.82, 0.400,     7.5},
 
@@ -846,52 +846,39 @@ local vwv_a1_skyraider =  {
     }, -- end of SFM_Data
 
 	--damage , index meaning see in  Scripts\Aircrafts\_Common\Damage.lua
-	Damage = {
-		[0] =  {critical_damage = 20, args = {296}},  -- pilot area
-		[1] =  {critical_damage = 20, args = {297}},  -- copilot area
-		[2] =  {critical_damage = 20, args = {146}},  -- nose bottom
-		[3] =  {critical_damage = 20, args = {235}},   -- elevator left
-		[4] =  {critical_damage = 20, args = {233}},   -- elevator right
-		[5] =  {critical_damage = 20, args = {213}},  -- pod left
-		[6] =  {critical_damage = 20, args = {223}},  -- pod right
-		[7] =  {critical_damage = 20, args = {159}},   -- Vertical stabilizer
-		[8] =  {critical_damage = 20, args = {152}},  -- fuselage bottom
-		[9] =  {critical_damage = 20, args = {154}},  -- cabin left
-		[10] =  {critical_damage = 20, args = {153}}, -- cabin right
-		[11] =  {critical_damage = 20, args = {156}}, -- under tail	
-		[12] =  {critical_damage = 20, args = {157}}, -- fuselage rear left
-		[13] =  {critical_damage = 20, args = {158}}, -- fuselage rear right
-		[12] =  {critical_damage = 20, args = {260}},  -- copilot area
-		[13] =  {critical_damage = 20, args = {261}},  -- nose bottom
-		[14] =  {critical_damage = 20, args = {262}},   -- elevator left
-		[15] =  {critical_damage = 20, args = {263}},   -- elevator right
-		[16] =  {critical_damage = 20, args = {264}},  -- pod left
-		[17] =  {critical_damage = 20, args = {265}},  -- pod right
-		[18] =  {critical_damage = 20, args = {266}},   -- Vertical stabilizer
+	Damage = verbose_to_dmg_properties({
+		["fuselage"]				= {critical_damage = 2, args =  { 65}},
+		["body"]			= {critical_damage = 3, args =  {146}},
+		["NOSE_RIGHT_SIDE"] 	= {critical_damage = 3, args =  {147}},
+		["NOSE_LEFT_SIDE"]		= {critical_damage = 3, args =  {150}},
+		["NOSE_BOTTOM"]			= {critical_damage = 3, args =  {148}},
+		["NOSE_TOP_SIDE"]		= {critical_damage = 3, args =  {147}},
 
-		-- Undercarriage
-		["WHEEL_REAR"] = {
-			critical_damage = 2, args = {134}, damage_boundary = 1.0,
-			construction = {durability = 0.15, skin = "Rubber", refractory = true},
-			innards = {
-				{id = "XUCShockC", skin = "Steel", wall = 0.010},
-			},
-		},
-		["WHEEL_R"] = {
-			critical_damage = 3, args = {135}, damage_boundary = 1.0,
-			construction = {durability = 0.25, skin = "Rubber", refractory = true},
-			innards = {
-				{id = "XUCShockRH", skin = "Steel", wall = 0.009},
-			},
-		},
-		["WHEEL_L"] = {
-			critical_damage = 3, args = {136}, damage_boundary = 1.0,
-			construction = {durability = 0.25, skin = "Rubber", refractory = true},
-			innards = {
-				{id = "XUCShockLH", skin = "Steel", wall = 0.009},
-			},
-		},
-	},
+		["WING_L"]			= {critical_damage = 3,args =  {223},deps_cells = {"FLAP_L_IN","WING_L_PART_OUT"}},
+		["WING_R"]			= {critical_damage = 3,args =  {213},deps_cells = {"FLAP_R_IN","WING_R_PART_OUT"}},
+		["CABIN"]		= {critical_damage = 3, args =  {221}},
+		["CANOPY"]		= {critical_damage = 3, args =  {231}},
+		["HOOK"]			= {critical_damage = 5, args =  {227}},
+		["FLAP_R_IN"]			= {critical_damage = 4, args =  {217}},
+
+		["FUSELAGE_BOTTOM"]		= {critical_damage = 4, args =  {152}},
+		["FUSELAGE_CENTR_TOP"]	= {critical_damage = 4, args =  {151}},
+		-- ["FUSELAGE_CENTR_L"]	= {critical_damage = 4, args =  {154}},
+		-- ["FUSELAGE_CENTR_R"]	= {critical_damage = 4, args =  {153}},
+
+		["rear_strut"]				= {critical_damage = 4, args =  {244}},
+		["RUDDER"]				= {critical_damage = 2, args =  {247}},
+
+		["Right_strut"]			= {critical_damage = 4, args =  {167}},
+		["left_strut"]			= {critical_damage = 3, args =  {161}},
+
+		["ELEVATOR"]		= {critical_damage = 3, args =  {235}},
+		["STABILIZER_R_IN"]		= {critical_damage = 3, args =  {233}},
+		["Front Wheel Out"]     = {critical_damage = 1},
+		["Left Wheel Out"]      = {critical_damage = 1},
+		["Right Wheel Out"]     = {critical_damage = 1},
+		}),
+
 	
 	DamageParts = 
 	{  
