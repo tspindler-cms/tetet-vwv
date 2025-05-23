8bf97b23154aefd0057d647ee0dd47077131f154
TeTeT Nimitz <tetet.nimitz@gmail.com>
2025-05-23 12:31:04 +0200

    Version bump to 1.1.0

--- TeTeT VWV/mods/aircraft/vwv_A-5/entry.lua
+++ TeTeT VWV/mods/aircraft/vwv_A-5/entry.lua
@@ -9,7 +9,7 @@ declare_plugin(self_ID,
 
 	fileMenuName = _("RA-5C Vigilante"),
 	update_id    = "ra5-vigilante",
-	version		 = "1.0.0",
+	version		 = "1.1.0",
 	state		 = "installed",
 	info		 = _("ChatGPT: The RA-5C Vigilante was a supersonic, carrier-based reconnaissance aircraft used by the U.S. Navy during the Cold War, known for its advanced sensors and sleek design."),
 	creditsFile = "credits.txt",
