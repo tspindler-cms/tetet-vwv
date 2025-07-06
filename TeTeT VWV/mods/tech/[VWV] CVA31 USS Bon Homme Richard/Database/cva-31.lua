4104f44e234ebe9afb70497e26e7255cae767ae5
TeTeT Nimitz <tetet.nimitz@gmail.com>
2025-07-06 22:02:11 +0200

    Rename CVA-31 to Bonnie Dick

--- TeTeT VWV/mods/tech/[VWV] CVA31 USS Bon Homme Richard/Database/cva-31.lua
+++ TeTeT VWV/mods/tech/[VWV] CVA31 USS Bon Homme Richard/Database/cva-31.lua
@@ -399,7 +399,7 @@ __LN.BR = {{ connector_name = 'POINT_GUN_5inch_7',
 
 GT.Name = "USS Bon Homme Richard" -- folder name for Liveries
 GT.DisplayName = _("[VWV] CVA-31 Bon Homme Richard") -- name in game in ME and on the tape at the bottom
-GT.DisplayNameShort = _("CVA-31") -- Label name
+GT.DisplayNameShort = _("Bonnie Dick") -- Label name
 GT.Rate = 27500.000000 
 
 GT.Sensors = {  OPTIC = {"long-range naval optics", "long-range naval LLTV", "long-range naval FLIR"}, --optics types
