fb12da7896d0258425d44136787748a83dd2ba1a
TeTeT Nimitz <tetet.nimitz@gmail.com>
2025-11-06 03:48:14 +0100

    Add rotor folding to CH-46D

---
+++ TeTeT VWV/mods/aircraft/[VWV] CH-46D/Liveries/vwv_ch46d/ch46d_early_folded/description.lua
@@ -0,0 +1,15 @@
+livery = {
+};
+
+name = "CH-46D Early [Folded]";
+
+order = 100;
+
+custom_args =
+{
+    [8] = 1.0, -- rotors folded
+    [36] = 1.0, -- ramp closed
+    [38] = 1.0, -- door closed
+    [1000] = 1.0, -- hide dispenser
+    [1003] = 0.0, -- winch stowed
+}
---
+++ TeTeT VWV/mods/aircraft/[VWV] CH-46D/Liveries/vwv_ch46d_late/ch46d_late_folded/description.lua
@@ -0,0 +1,15 @@
+livery = {
+};
+
+name = "CH-46D Late (dispensers) [Folded]";
+
+order = 1;
+
+custom_args =
+{
+    [8] = 1.0, -- rotors folded
+    [36] = 1.0, -- ramp closed
+    [38] = 0.0, -- door closed
+    [1000] = 0.0, -- Show dispenser
+    [1003] = 0.0, -- winch stowed
+}
