c70074e1286f2831c453cdb2c47a760fbe93ad78
TeTeT Nimitz <tetet.nimitz@gmail.com>
2025-11-08 10:46:00 +0100

    Update of buildings and EC-121
      Buildings
        - Attempt to fix the armed VC Guard Tower
      EC-121
        - Reduce normal texture strengths, thanks to Urbi

--- TeTeT VWV/mods/tech/[VWV] buildings/Database/fortifications/vwv_vn_guardTower.lua
+++ TeTeT VWV/mods/tech/[VWV] buildings/Database/fortifications/vwv_vn_guardTower.lua
@@ -26,13 +26,13 @@ GT.WS.maxTargetDetectionRange = 2500;
 
 GT.WS.fire_on_march = false;
 
-
 local ws = GT_t.inc_ws();
 GT.WS[ws] = {};
 GT.WS[ws].center = 'CENTER_TOWER_01';
 GT.WS[ws].angles = {
-	{math.rad(-60), math.rad(60), math.rad(-60), math.rad(80)},
+	{math.rad(60), math.rad(-60), math.rad(-60), math.rad(80)},
 };
+GT.WS[ws].reference_angle_Y = math.rad(0);
 GT.WS[ws].drawArgument1 = 0;
 GT.WS[ws].drawArgument2 = 1;
 GT.WS[ws].omegaY = math.rad(90);
@@ -41,17 +41,18 @@ GT.WS[ws].reloadAngleY = math.rad(30);
 GT.WS[ws].reloadAngleZ = math.rad(30);
 GT.WS[ws].stabilizer = false;
 GT.WS[ws].laser = false;
-GT.WS[ws].pointer = 'POINT_762mm_1';
+-- GT.WS[ws].pointer = 'POINT_SIGHT_1';
 
-__LN = add_launcher(GT.WS[ws], GT_t.LN_t.vap_vc_ak47n);
-__LN.maxShootingSpeed = 0;
-for i=2,8 do -- 8 clips, 240 rounds
-	__LN.PL[i] = {};
-	set_recursive_metatable(__LN.PL[i], __LN.PL[1]);
+local ln = add_launcher(GT.WS[ws], GT_t.LN_t.vap_vc_ak47n)
+ln.maxShootingSpeed = 0
+for i=2,8 do
+    ln.PL[i] = {}
+    set_recursive_metatable(ln.PL[i], ln.PL[1])
 end
-__LN.fireAnimationArgument = 23;
-__LN.BR[1].connector_name = 'POINT_762mm_1'
-__LN.customViewPoint = { "IronSight/IronSight", {0.0, 0.0, 0.0} };
+ln.fireAnimationArgument = 23
+ln.BR = { { connector_name = 'POINT_762mm_1' } }
+-- ln.BR[1].connector_name = 'POINT_762mm_1'
+ln.customViewPoint = { "IronSight/IronSight", {0.0, 0.0, 0.0} }
 
 ws = GT_t.inc_ws();
 GT.WS[ws] = {};
@@ -68,17 +69,18 @@ GT.WS[ws].reloadAngleY = math.rad(30);
 GT.WS[ws].reloadAngleZ = math.rad(30);
 GT.WS[ws].stabilizer = false;
 GT.WS[ws].laser = false;
-GT.WS[ws].pointer = 'POINT_762mm_2';
+-- GT.WS[ws].pointer = 'POINT_SIGHT_2';
 
-__LN = add_launcher(GT.WS[ws], GT_t.LN_t.vap_vc_ak47n);
-__LN.maxShootingSpeed = 0;
-for i=2,8 do -- 8 clips, 240 rounds
-	__LN.PL[i] = {};
-	set_recursive_metatable(__LN.PL[i], __LN.PL[1]);
+local ln = add_launcher(GT.WS[ws], GT_t.LN_t.vap_vc_ak47n)
+ln.maxShootingSpeed = 0
+for i=2,8 do
+    ln.PL[i] = {}
+    set_recursive_metatable(ln.PL[i], ln.PL[1])
 end
-__LN.fireAnimationArgument = 23;
-__LN.BR[1].connector_name = 'POINT_762mm_2'
-__LN.customViewPoint = { "IronSight/IronSight", {0.0, 0.0, 0.0} };
+ln.fireAnimationArgument = 24
+ln.BR = { { connector_name = 'POINT_762mm_2' } }
+-- ln.BR[1].connector_name = 'POINT_762mm_2'
+ln.customViewPoint = { "IronSight/IronSight", {0.0, 0.0, 0.0} }
 
 ws = GT_t.inc_ws();
 GT.WS[ws] = {};
@@ -95,17 +97,18 @@ GT.WS[ws].reloadAngleY = math.rad(30);
 GT.WS[ws].reloadAngleZ = math.rad(30);
 GT.WS[ws].stabilizer = false;
 GT.WS[ws].laser = false;
-GT.WS[ws].pointer = 'POINT_762mm_3';
+-- GT.WS[ws].pointer = 'POINT_SIGHT_3';
 
-__LN = add_launcher(GT.WS[ws], GT_t.LN_t.vap_vc_ak47n);
-__LN.maxShootingSpeed = 0;
-for i=2,8 do -- 8 clips, 240 rounds
-	__LN.PL[i] = {};
-	set_recursive_metatable(__LN.PL[i], __LN.PL[1]);
+local ln = add_launcher(GT.WS[ws], GT_t.LN_t.vap_vc_ak47n)
+ln.maxShootingSpeed = 0
+for i=2,8 do
+    ln.PL[i] = {}
+    set_recursive_metatable(ln.PL[i], ln.PL[1])
 end
-__LN.fireAnimationArgument = 23;
-__LN.BR[1].connector_name = 'POINT_762mm_3'
-__LN.customViewPoint = { "IronSight/IronSight", {0.0, 0.0, 0.0} };
+ln.fireAnimationArgument = 25
+-- ln.BR[1].connector_name = 'POINT_762mm_3'
+ln.BR = { { connector_name = 'POINT_762mm_3' } }
+ln.customViewPoint = { "IronSight/IronSight", {0.0, 0.0, 0.0} }
 
 ws = GT_t.inc_ws();
 GT.WS[ws] = {};
@@ -113,7 +116,7 @@ GT.WS[ws].center = 'CENTER_TOWER_04';
 GT.WS[ws].angles = {
 	{math.rad(-60), math.rad(60), math.rad(-60), math.rad(80)},
 };
-GT.WS[ws].reference_angle_Y = math.rad(270)					
+GT.WS[ws].reference_angle_Y = math.rad(270)
 GT.WS[ws].drawArgument1 = 6;
 GT.WS[ws].drawArgument2 = 7;
 GT.WS[ws].omegaY = math.rad(90);
@@ -122,17 +125,18 @@ GT.WS[ws].reloadAngleY = math.rad(30);
 GT.WS[ws].reloadAngleZ = math.rad(30);
 GT.WS[ws].stabilizer = false;
 GT.WS[ws].laser = false;
-GT.WS[ws].pointer = 'POINT_762mm_4';
+-- GT.WS[ws].pointer = 'POINT_SIGHT_4';
 
-__LN = add_launcher(GT.WS[ws], GT_t.LN_t.vap_vc_ak47n);
-__LN.maxShootingSpeed = 0;
-for i=2,8 do -- 8 clips, 240 rounds
-	__LN.PL[i] = {};
-	set_recursive_metatable(__LN.PL[i], __LN.PL[1]);
+local ln = add_launcher(GT.WS[ws], GT_t.LN_t.vap_vc_ak47n)
+ln.maxShootingSpeed = 0
+for i=2,8 do
+    ln.PL[i] = {}
+    set_recursive_metatable(ln.PL[i], ln.PL[1])
 end
-__LN.fireAnimationArgument = 23;
-__LN.BR[1].connector_name = 'POINT_762mm_4'
-__LN.customViewPoint = { "IronSight/IronSight", {0.0, 0.0, 0.0} };
+ln.fireAnimationArgument = 26
+ln.BR = { { connector_name = 'POINT_762mm_4' } }
+-- ln.BR[1].connector_name = 'POINT_762mm_4'
+ln.customViewPoint = { "IronSight/IronSight", {0.0, 0.0, 0.0} }
 
 GT.Name = "vwv_vn_guardTower"
 GT.DisplayName = _("[VWV] VC Guard Tower (armed)")
