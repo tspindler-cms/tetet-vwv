9f353901578462150eb901b90fec2dc23f2966fe
TeTeT Nimitz <tetet.nimitz@gmail.com>
2026-01-19 19:01:40 +0100

    Update New Jersey by James and Woogey
      USS New Jersey
        - New livery by Woogey
        - Improve config and model by James

---
+++ TeTeT VWV/mods/tech/[VWV] BB62 USS New Jersey/Liveries/USS New Jersey/BB-62/description.lua
@@ -0,0 +1,8 @@
+livery = {
+}
+
+name = "BB-62 USS New Jersey"
+
+countries = {
+    "USA",
+}
---
+++ TeTeT VWV/mods/tech/[VWV] BB62 USS New Jersey/Liveries/USS New Jersey/BB-62_1/description.lua
@@ -0,0 +1,12 @@
+livery = {
+    {"nj_5in38_mk32_mod12", 0, "nj_5in38_mk32_mod12.dds", false};
+    {"nj_16in50_mk7", 0, "nj_16in50_mk7.dds", false};
+    {"nj_detail", 0, "nj_detail.dds", false};
+    {"nj_hull", 0, "nj_hull.dds", false};
+}
+
+name = "BB-62 USS New Jersey"
+
+countries = {
+    "USA",
+}
