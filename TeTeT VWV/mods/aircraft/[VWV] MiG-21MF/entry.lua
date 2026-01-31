aa7dd64ea2075c32927dd252ec7303f412e8724d
HawaiianRyan808 <noone@theplace.com>
2026-01-31 11:02:21 -0600

    Clean up formatting of entry.lua and update version

--- TeTeT VWV/mods/aircraft/[VWV] MiG-21MF/entry.lua
+++ TeTeT VWV/mods/aircraft/[VWV] MiG-21MF/entry.lua
@@ -6,7 +6,7 @@ declare_plugin(self_ID, {
     fileMenuName = _("MiG-21MF"),
     developerName = _("TeTeT"),
     update_id = "mig21mf",
-    version = "2.4.0",
+    version = "3.0.0",
     state = "installed",
     info = _("Wikipedia: Export variant of the MiG-21S with two major differences: the RP-22 radar of the MiG-21S was substituted with the older RP-21MA radar, and featured a built-in GSh-23L cannon instead of a cannon pod"),
     creditsFile = "credits.txt",
@@ -33,10 +33,10 @@ declare_plugin(self_ID, {
 })
 
 -------------------------------------------------------------------------------------
-mount_vfs_model_path(current_mod_path .. "/Shapes")
+mount_vfs_model_path(current_mod_path    .. "/Shapes")
 mount_vfs_liveries_path(current_mod_path .. "/Liveries")
-mount_vfs_texture_path(current_mod_path .. "/Textures/mig21mf")
+mount_vfs_texture_path(current_mod_path  .. "/Textures/mig21mf")
 -------------------------------------------------------------------------------------
-add_aircraft(dofile(current_mod_path .. '/Database/mig21mf.lua'))
+add_aircraft( dofile(current_mod_path .. "/Database/mig21mf.lua") )
 -------------------------------------------------------------------------------------
 plugin_done()
