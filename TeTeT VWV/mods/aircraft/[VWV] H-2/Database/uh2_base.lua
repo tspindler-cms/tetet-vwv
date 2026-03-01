72a92b75b8045ef11228fc66d37dcfc1db013573
HawaiianRyan808 <64971443+hawaiianryan808@users.noreply.github.com>
2026-03-01 11:16:25 -0600

    Update uh2_base.lua
    Fix the tail rotor rotation direction for the .fbx rotor blur.

--- TeTeT VWV/mods/aircraft/[VWV] H-2/Database/uh2_base.lua
+++ TeTeT VWV/mods/aircraft/[VWV] H-2/Database/uh2_base.lua
@@ -666,7 +666,7 @@ return {
         tail_rotor = {
             blades_count    = 3,
             modelBlade_FBX  = "/models/sh2f_blade.fbx",
-            rotor_direction = 1,
+            rotor_direction = -1,
             rotor_locations = {{pos = {-7.2187, 1.4516, -0.4933}, pitch = 0.0, yaw = math.rad(90)}},
         },
     },
@@ -871,3 +871,4 @@ return {
 		},
     },
 };
+
