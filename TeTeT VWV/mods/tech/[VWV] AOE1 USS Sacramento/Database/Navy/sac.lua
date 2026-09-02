545d8303c59c2bea3d52e82be88caf5e36d98e4b
HawaiianRyan808 <64971443+hawaiianryan808@users.noreply.github.com>
2026-09-02 15:32:03 -0500

    Update sac.lua
    Fixed a lot of code style issues, mostly adding semicolons for consistency.

--- TeTeT VWV/mods/tech/[VWV] AOE1 USS Sacramento/Database/Navy/sac.lua
+++ TeTeT VWV/mods/tech/[VWV] AOE1 USS Sacramento/Database/Navy/sac.lua
@@ -4,29 +4,29 @@ GT = {};
 dofile(current_mod_path..'/Database/scripts/sac_RunwaysAndRoutes.lua') -- the runways and taxiway lua file
 GT_t.ws = 0;
 
-set_recursive_metatable(GT, GT_t.generic_ship) -- all ships have this
+set_recursive_metatable(GT, GT_t.generic_ship); -- all ships have this
 
-GT.visual = {}
-GT.visual.shape = "sac_dcs"  -- name of LODs lua
-GT.visual.shape_dstr = "" -- if there is a destruction model it goes here
+GT.visual = {};
+GT.visual.shape = "sac_dcs";  -- name of LODs lua
+GT.visual.shape_dstr = ""; -- if there is a destruction model it goes here
 
 GT.life = 7300; -- hit points
 GT.mass = 53000000; -- KGs
-GT.max_velocity = 16.719 --M/S
-GT.race_velocity = 12.1 --M/S
-GT.economy_velocity = 7.7 --M/S 
-GT.economy_distance = 13000 -- KM
-GT.race_distance = 2.778e+006
-GT.shipLength = 218.39 -- o.a in meters
-GT.Width = 33 --meters
-GT.Height = 42.136 --from sea level I think
-GT.Length = 243.0 -- deck length I think
-GT.DeckLevel = 9.2974 -- height of deck
-GT.X_nose = 113.41 -- bow wave
-GT.X_tail = -96 -- wake
-GT.Tail_Width = 38 --wake width
-GT.Gamma_max = 0.35 -- no idea
-GT.Om = 0.05 --no idea
+GT.max_velocity = 16.719; --M/S
+GT.race_velocity = 12.1; --M/S
+GT.economy_velocity = 7.7; --M/S 
+GT.economy_distance = 13000; -- KM
+GT.race_distance = 2.778e+006;
+GT.shipLength = 218.39; -- o.a in meters
+GT.Width = 33; --meters
+GT.Height = 42.136; --from sea level I think
+GT.Length = 243.0; -- deck length I think
+GT.DeckLevel = 9.2974; -- height of deck
+GT.X_nose = 113.41; -- bow wave
+GT.X_tail = -96; -- wake
+GT.Tail_Width = 38; --wake width
+GT.Gamma_max = 0.35; -- no idea
+GT.Om = 0.05; --no idea
 GT.speedup = 0.119249; --time it takes to accelerate
 GT.R_min = 300; -- turn radius
 
@@ -84,9 +84,9 @@ GT.airFindDist = 130000; -- Max detection range air threats (meters)
 
 
 --Radar info
-GT.WS = {}
+GT.WS = {};
 GT.WS.maxTargetDetectionRange = 56000;
-GT.WS.radar_type = 102 --optical in wstypes
+GT.WS.radar_type = 102; --optical in wstypes
 GT.WS.searchRadarMaxElevation = math.rad(60);
 GT.WS.searchRadarFrequencies = {{50.0e6, 54.0e6}, {2.0e9, 2.2e9}};
 local ws;
@@ -117,21 +117,21 @@ GT_t.WS_t.ship_mk33_76mm = { name = "Mk33 3 inch"};
 GT_t.WS_t.ship_mk33_76mm.angles = {
 	{math.rad(180), math.rad(-180), math.rad(-15), math.rad(85)},
 	};
-GT_t.WS_t.ship_mk33_76mm.omegaY = math.rad(10) -- Block 1B
-GT_t.WS_t.ship_mk33_76mm.omegaZ = math.rad(10) -- Block 1B
+GT_t.WS_t.ship_mk33_76mm.omegaY = math.rad(10); -- Block 1B
+GT_t.WS_t.ship_mk33_76mm.omegaZ = math.rad(10); -- Block 1B
 GT_t.WS_t.ship_mk33_76mm.pidY = {p=300, i = 0.05, d = 10.0, inn = 1000};
 GT_t.WS_t.ship_mk33_76mm.pidZ = {p=300, i = 0.05, d = 10.0, inn = 1000};
-GT_t.WS_t.ship_mk33_76mm.reference_angle_Z = 0
+GT_t.WS_t.ship_mk33_76mm.reference_angle_Z = 0;
 
 GT_t.LN_t.ship_mk33_76mm_AIR = {};
-GT_t.LN_t.ship_mk33_76mm_AIR.type = 11
-GT_t.LN_t.ship_mk33_76mm_AIR.distanceMin = 20
-GT_t.LN_t.ship_mk33_76mm_AIR.distanceMax = 13341
-GT_t.LN_t.ship_mk33_76mm_AIR.max_trg_alt = 9083
-GT_t.LN_t.ship_mk33_76mm_AIR.reactionTime = 2
-GT_t.LN_t.ship_mk33_76mm_AIR.sensor = {}
-set_recursive_metatable(GT_t.LN_t.ship_mk33_76mm_AIR.sensor, GT_t.WSN_t[20])
-GT_t.LN_t.ship_mk33_76mm_AIR.PL = {}
+GT_t.LN_t.ship_mk33_76mm_AIR.type = 11;
+GT_t.LN_t.ship_mk33_76mm_AIR.distanceMin = 20;
+GT_t.LN_t.ship_mk33_76mm_AIR.distanceMax = 13341;
+GT_t.LN_t.ship_mk33_76mm_AIR.max_trg_alt = 9083;
+GT_t.LN_t.ship_mk33_76mm_AIR.reactionTime = 2;
+GT_t.LN_t.ship_mk33_76mm_AIR.sensor = {};
+set_recursive_metatable(GT_t.LN_t.ship_mk33_76mm_AIR.sensor, GT_t.WSN_t[20]);
+GT_t.LN_t.ship_mk33_76mm_AIR.PL = {};
 GT_t.LN_t.ship_mk33_76mm_AIR.PL[1] = {
 							shell_name = {"jjj_sac_mk33_76mm_AIR"},
 							automaticLoader = false,
@@ -150,18 +150,21 @@ GT.WS[ws].area = 'T1';
 GT.WS[ws].center = 'T1M';
 GT.WS[ws].drawArgument1 = 1111;
 GT.WS[ws].drawArgument2 = 1112;
-GT.WS[ws].reference_angle_Y = math.rad(0)
+GT.WS[ws].reference_angle_Y = math.rad(0);
 GT.WS[ws].angles = {
                     {math.rad(130), math.rad(-57), math.rad(0), math.rad(85)},
                     };
 __LN = add_launcher(GT.WS[ws], GT_t.LN_t.ship_mk33_76mm_AIR);
 __LN.sightMaxTanVel = 300;
-__LN.BR = {{ connector_name = 'POINT_MK33_1L',
+__LN.BR = {
+		   { connector_name = 'POINT_MK33_1L',
 			recoilArgument = 1113,
 			recoilTime = 0.4 },
+	
 		   { connector_name = 'POINT_MK33_1R',
 			recoilArgument = 1114,
-			recoilTime = 0.4 }};
+			recoilTime = 0.4 },
+};
 
 -------------------------------------------------------------------------------------------------
 --   3inch turret 2
@@ -174,18 +177,20 @@ GT.WS[ws].area = 'T2';
 GT.WS[ws].center = 'T2M';
 GT.WS[ws].drawArgument1 = 1121;
 GT.WS[ws].drawArgument2 = 1122;
-GT.WS[ws].reference_angle_Y = math.rad(-0)
+GT.WS[ws].reference_angle_Y = math.rad(-0);
 GT.WS[ws].angles = {
                     {math.rad(57), math.rad(-130), math.rad(0), math.rad(85)},
                     };
 __LN = add_launcher(GT.WS[ws], GT_t.LN_t.ship_mk33_76mm_AIR);
 __LN.sightMaxTanVel = 300;
-__LN.BR = {{ connector_name = 'POINT_MK33_2L',
+__LN.BR = {
+		   { connector_name = 'POINT_MK33_2L',
 			recoilArgument = 1123,
 			recoilTime = 0.4 },
 		   { connector_name = 'POINT_MK33_2R',
 			recoilArgument = 1124,
-			recoilTime = 0.4 }};
+			recoilTime = 0.4 },
+};
 -------------------------------------------------------------------------------------------------
 --   3inch turret 3
 -------------------------------------------------------------------------------------------------
@@ -197,18 +202,20 @@ GT.WS[ws].area = 'T3';
 GT.WS[ws].center = 'T3M';
 GT.WS[ws].drawArgument1 = 1131;
 GT.WS[ws].drawArgument2 = 1132;
-GT.WS[ws].reference_angle_Y = math.rad(-180)
+GT.WS[ws].reference_angle_Y = math.rad(-180);
 GT.WS[ws].angles = {
                     {math.rad(-120), math.rad(52), math.rad(0), math.rad(85)},
                     };
 __LN = add_launcher(GT.WS[ws], GT_t.LN_t.ship_mk33_76mm_AIR);
 __LN.sightMaxTanVel = 300;
-__LN.BR = {{ connector_name = 'POINT_MK33_3L',
+__LN.BR = {
+		   { connector_name = 'POINT_MK33_3L',
 			recoilArgument = 1133,
 			recoilTime = 0.4 },
 		   { connector_name = 'POINT_MK33_3R',
 			recoilArgument = 1134,
-			recoilTime = 0.4 }};
+			recoilTime = 0.4 },
+};
 
 -------------------------------------------------------------------------------------------------
 --   3inch turret 4
@@ -221,22 +228,24 @@ GT.WS[ws].area = 'T4';
 GT.WS[ws].center = 'T4M';
 GT.WS[ws].drawArgument1 = 1141;
 GT.WS[ws].drawArgument2 = 1142;
-GT.WS[ws].reference_angle_Y = math.rad(180)
+GT.WS[ws].reference_angle_Y = math.rad(180);
 GT.WS[ws].angles = {
                     {math.rad(-52), math.rad(120), math.rad(0), math.rad(85)},
                     };
 __LN = add_launcher(GT.WS[ws], GT_t.LN_t.ship_mk33_76mm_AIR);
 __LN.sightMaxTanVel = 300;
-__LN.BR = {{ connector_name = 'POINT_MK33_4L',
+__LN.BR = {
+		   { connector_name = 'POINT_MK33_4L',
 			recoilArgument = 1143,
 			recoilTime = 0.4 },
 		   { connector_name = 'POINT_MK33_4R',
 			recoilArgument = 1144,
-			recoilTime = 0.4 }};
+			recoilTime = 0.4 },
+};
 
 --------------------------- SAC_radar ------------------------------------------
-ws = GT_t.inc_ws()
-local first_SAC_tracker_id = ws
+ws = GT_t.inc_ws();
+local first_SAC_tracker_id = ws;
 GT.WS[ws] = {
 	area = 'Mast',
 	center = 'RADAR1',
@@ -262,9 +271,9 @@ GT.WS[ws] = {
     }
 }
 
-local SAC_TRACKERS = {{{'self', ws}}}
+local SAC_TRACKERS = {{{'self', ws}}};
 
-for i=2,13 do 
+for i = 2, 13 do 
     ws = GT_t.inc_ws()
     GT.WS[ws] = {}
     set_recursive_metatable(GT.WS[ws], GT.WS[first_SAC_tracker_id])
@@ -272,36 +281,36 @@ for i=2,13 do
 end
 --------------------------- SAC_SPG49 tracker radar ------------------------------------------
 ws = GT_t.inc_ws();
-GT.WS[ws] = {}
+GT.WS[ws] = {};
 set_recursive_metatable(GT.WS[ws], GT_t.SS_t.VYMPEL_TRACKER[1]);
-GT.WS[ws].reference_angle_Y = math.rad(0)
-GT.WS[ws].reference_angle_Z = math.rad(0)
-GT.WS[ws].center = 'FCS1'
+GT.WS[ws].reference_angle_Y = math.rad(0);
+GT.WS[ws].reference_angle_Z = math.rad(0);
+GT.WS[ws].center = 'FCS1';
 GT.WS[ws].angles = {
                     {math.rad(180), math.rad(-180), math.rad(-10), math.rad(80)},
                     };
-GT.WS[ws].drawArgument1 = 1031
+GT.WS[ws].drawArgument1 = 1031;
 GT.WS[ws].LN[1].reactionTime = 1;
 GT.WS[ws].LN[1].distanceMin = 400;
 GT.WS[ws].LN[1].distanceMax = 180000;
 GT.WS[ws].LN[1].min_trg_alt = 0;
 GT.WS[ws].LN[1].max_trg_alt = 24400;
 ws = GT_t.inc_ws();
-GT.WS[ws] = {}
+GT.WS[ws] = {};
 set_recursive_metatable(GT.WS[ws], GT_t.SS_t.VYMPEL_TRACKER[2]);
 GT.WS[ws].base = ws-1;
 local visual_tracker_ws1 = {{{"self", ws-1}}, {{"self", ws}}};
 				
 -------------------------------------------------------------------------
 
-GT.Name = "USS Sacramento 1968" -- folder name for Liveries
-GT.DisplayName = _("[VWV] AOE-1 USS Sacramento") -- name in game in ME and on the tape at the bottom
-GT.DisplayNameShort = _("Sacramento class") -- Label name
-GT.Rate = 53000.000000 
+GT.Name = "USS Sacramento 1968"; -- folder name for Liveries
+GT.DisplayName = _("[VWV] AOE-1 USS Sacramento"); -- name in game in ME and on the tape at the bottom
+GT.DisplayNameShort = _("Sacramento class"); -- Label name
+GT.Rate = 53000.000000;
 
-GT.Sensors = {  OPTIC = {"long-range naval optics", "long-range naval LLTV", "long-range naval FLIR", "long-range air defence optics"}, --optics types
-				RADAR = {"SAC air",
-                    "SAC surface",}, --radar types
+GT.Sensors = {
+				OPTIC = {"long-range naval optics", "long-range naval LLTV", "long-range naval FLIR", "long-range air defence optics"}, --optics types
+				RADAR = {"SAC air", "SAC surface"}, --radar types
 			};
 GT.sensor = {};
 set_recursive_metatable(GT.sensor, GT_t.SN_visual);
@@ -322,7 +331,7 @@ GT.attribute		= {wsType_Navy,wsType_Ship,wsType_ArmedShip,wsType_GenericLightArm
 					--unfortunately the list with these have since been hidden by ED along with the ships lua files
 GT.Categories		= {
 	{name = "Armed Ship"}, -- has weapons
-	{name = "HelicopterCarrier"}
+	{name = "HelicopterCarrier"},
 };
 				
 -- categories in the mission editor 
